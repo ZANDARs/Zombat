273f2bc8f5abec6b29ba7d5f3cf71e8b6d7e5455
zandar <jokez.tik.bs@gmail.com>
2024-08-16 18:53:33 +0300

    Added a zombie, its display on the screen and beta versions of random coordinates and random textures

--- main.py
+++ main.py
@@ -1,6 +1,28 @@
 from objects import *
 
 player = Player(100, 100, 5, player_image, 25, 25)
+x = 100
+y = 200
+
+def random_cord():
+    global x
+    global y
+    cr = random.randint(1, 4)
+    if cr == 1:
+        x = 10
+        y = random.randint(1, 500)
+    elif cr == 2:
+        x = 700 - 10
+        y = random.randint(1, 500)
+    elif cr == 3:
+        x = random.randint(1, 700)
+        y = 10
+    elif cr == 4:
+        x = random.randint(1, 700)
+        y = 500 - 10
+random_cord()
+player = Player(100, 100, 5, player_image, 25, 25)
+zomb = Zombie(x, y, 2, random.choice(["textures/zomb_1.png", "textures/zomb_2.png", "textures/zomb_3.png"]), 25, 25)
 
 game = True
 
@@ -16,6 +38,8 @@ while True:
         player.draw()
         bullets.update()
         bullets.draw(win)
+        zomb.movement()
+        zomb.draw()
 #        bullet.fly()
  #       bullet.draw()
 
