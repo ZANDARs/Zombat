c6c06256bf7784280d8abf9c9faef6bfcd08beba
zandar <jokez.tik.bs@gmail.com>
2024-08-17 14:33:24 +0300

    Reworked zombie spawn

--- main.py
+++ main.py
@@ -4,27 +4,16 @@ player = Player(100, 100, 5, player_image, 25, 25)
 x = 100
 y = 200
 
-def random_cord():
-    global x
-    global y
-    cr = random.randint(1, 4)
-    if cr == 1:
-        x = 10
-        y = random.randint(1, 500)
-    elif cr == 2:
-        x = 700 - 10
-        y = random.randint(1, 500)
-    elif cr == 3:
-        x = random.randint(1, 700)
-        y = 10
-    elif cr == 4:
-        x = random.randint(1, 700)
-        y = 500 - 10
-random_cord()
+
 player = Player(100, 100, 5, player_image, 25, 25)
-zomb = Zombie(x, y, 2, random.choice(["textures/zomb_1.png", "textures/zomb_2.png", "textures/zomb_3.png"]), 25, 25)
+
 
 game = True
+
+for i in range (15):
+    zomb = Zombie(-10, 500, 5, zombie_images[0], 25, 25)
+    zomb.spawn()
+    enemmies.add(zomb)
 
 while True:
     for event in pyg.event.get():
@@ -34,12 +23,19 @@ while True:
 
     if game:
         win.blit(background_img, (0, 0))
+        for zomb in enemmies:
+            dx = zomb.rect.centerx - player.rect.centerx
+            dy = player.rect.centery - zomb.rect.centery
+            ang = -math.atan2(dy, dx) - math.pi
+
+            zomb.movement(ang)
+            zomb.draw()
+
+
         player.update()
         player.draw()
         bullets.update()
         bullets.draw(win)
-        zomb.movement()
-        zomb.draw()
 #        bullet.fly()
  #       bullet.draw()
 
