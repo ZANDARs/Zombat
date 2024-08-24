4d55732eeb6cf008ac5e64e61d04273fff8162e6
zandar <jokez.tik.bs@gmail.com>
2024-08-24 15:37:54 +0300

    Imported font, added sound and color changes, added rectangular interface

--- setings.py
+++ setings.py
@@ -1,6 +1,8 @@
 import pygame
 import random
 
+from pygame import font
+
 pyg = pygame
 pyg.init()
 
@@ -9,12 +11,14 @@ win_height = 500
 
 FPS = 20
 
-win = pyg.display.set_mode((win_width, win_height))
+font_ = font.Font(None, 65)
+
+win = pyg.display.set_mode((win_width, win_height + 50))
 
 clock = pyg.time.Clock()
 player_image = "textures/player.png"
 
-background_img = pyg.transform.scale(pyg.image.load("textures/background.png"), (700, 500))
+background_img = pyg.transform.scale(pyg.image.load("textures/background.png"), (win_width, win_height))
 
 bullet_image = "textures/bullet.png"
 
@@ -25,5 +29,12 @@ zombie_images = ["textures/zomb_1.png", "textures/zomb_2.png", "textures/zomb_3.
 
 
 fire_sound = None
+damage_sound = None
+coin = None
 bullets = pyg.sprite.Group()
 enemmies = pyg.sprite.Group()
+
+interface_rect = pyg.Rect(0, win_height, win_width, 50)
+interface_color = (150, 150, 100)
+hp_color = (249, 0, 0)
+zomb_kill_color = (34, 154, 0)
