5806fe704ae325b1dec81a2c61d5c373aa2a2da8
zandar <jokez.tik.bs@gmail.com>
2024-08-24 15:41:59 +0300

    Added a counter of killed zombies, and its display in the interface together with the player's hp

--- main.py
+++ main.py
@@ -3,9 +3,10 @@ from objects import *
 player = Player(100, 100, 5, player_image, 25, 25)
 x = 100
 y = 200
-
+zomb_k = 0
 
 player = Player(100, 100, 5, player_image, 25, 25)
+
 
 
 game = True
@@ -31,20 +32,32 @@ while True:
             zomb.movement(ang)
             zomb.draw()
 
+            if player.hitbox.colliderect(zomb.hitbox):
+                player.hp -= 20
+                zomb.spawn()
 
         player.update()
         player.draw()
         bullets.update()
         bullets.draw(win)
 
-        for bullet in bullets:
-            hits = pygame.sprite.spritecollide(bullet, enemmies, True)
-            if hits:
-                print("test")
-                # Додаткові дії при зіткненні (наприклад, видалення кулі або зомбі)
-                bullets.remove(bullet)
+        collide = pyg.sprite.groupcollide(bullets, enemmies, True, False)
+        for bullet in collide:
+            for zomb in collide[bullet]:
+                zomb.hp -= 1
+                if zomb.hp <= 0:
+                    zomb.spawn()
+                    zomb_k += 1
 #        bullet.fly()
  #       bullet.draw()
+
+        pyg.draw.rect(win, interface_color, interface_rect)
+        hp_bar = f"{player.hp}/{player.max_hp}"
+        text_hp = font_.render(hp_bar, False, hp_color)
+        win.blit(text_hp, (interface_rect.x + 10, interface_rect.y + 10))
+        txt_zomb_k = font_.render(str(zomb_k), True, zomb_kill_color)
+        win.blit(txt_zomb_k, (interface_rect.x + 600, interface_rect.y + 10))
+
 
     pyg.display.update()
     clock.tick(FPS)
